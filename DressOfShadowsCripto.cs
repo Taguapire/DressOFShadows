861941470d036a990298eead2938cd29d29733a4
Luis Vasquez <sqlbiker@protonmail.com>
2022-01-10 02:27:31 -0300

    Add project files.

---
+++ DressOfShadowsCripto.cs
@@ -0,0 +1,102 @@
+﻿using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Utilities.Encoders;
+using System;
+using System.Text;
+
+namespace DressOfShadows
+{
+    /// <summary>
+    /// Class that provides blowfish encryption.
+    /// </summary>
+    /// 
+
+    public class DressOfShadowsCripto
+    {
+        private static readonly Encoding Encoding = Encoding.UTF8;
+
+        public DressOfShadowsCripto()
+        {
+        }
+
+        public string Encriptar(string pClave, string pTextoNormal)
+        {
+            return BlowfishEncrypt(pTextoNormal, pClave);
+        }
+
+        public string DesEncriptar(string pClave, string pTextoEncriptado)
+        {
+            return BlowfishDecrypt(pTextoEncriptado, pClave);
+        }
+
+        private string BlowfishEncrypt(string strValue, string key)
+        {
+            try
+            {
+                BlowfishEngine engine = new();
+
+                PaddedBufferedBlockCipher cipher = new(engine);
+
+                KeyParameter keyBytes = new(Encoding.GetBytes(key));
+
+                cipher.Init(true, keyBytes);
+
+                byte[] inB = Encoding.GetBytes(strValue);
+
+                byte[] outB = new byte[cipher.GetOutputSize(inB.Length)];
+
+                int len1 = cipher.ProcessBytes(inB, 0, inB.Length, outB, 0);
+
+                cipher.DoFinal(outB, len1);
+
+                return BitConverter.ToString(outB).Replace("-", "");
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        private string BlowfishDecrypt(string name, string keyString)
+        {
+            BlowfishEngine engine = new();
+            PaddedBufferedBlockCipher cipher = new(engine);
+
+            StringBuilder result = new();
+
+            cipher.Init(false, new KeyParameter(Encoding.GetBytes(keyString)));
+
+            byte[] out1 = Hex.Decode(name);
+            byte[] out2 = new byte[cipher.GetOutputSize(out1.Length)];
+
+            int len2 = cipher.ProcessBytes(out1, 0, out1.Length, out2, 0);
+
+            cipher.DoFinal(out2, len2); //Pad block corrupted error happens here
+
+            string s2 = BitConverter.ToString(out2);
+
+            for (int i = 0; i < s2.Length; i++)
+            {
+                char c = s2[i];
+                if (c != 0)
+                {
+                    result.Append(c.ToString());
+                }
+            }
+
+            return FromHexString(result.ToString().Replace("-", ""));
+        }
+
+        private string FromHexString(string hexString)
+        {
+            byte[] bytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return Encoding.GetString(bytes);
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DressOfShadows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //private Blowfish DressOfShadowsBlackBox;
+        private string Global_Compare;
+        private string TextoCodigo;
+        private string TextoEntrada;
+        private string TextoSalida;
+        private DressOfShadowsCripto OperacionCripto;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Muestra Password
+        private void ClibkBtnVerPassword(object sender, RoutedEventArgs e)
+        {
+            if (TBoxClaveEntrada.PasswordRevealMode == PasswordRevealMode.Peek)
+            {
+                TBoxClaveEntrada.PasswordRevealMode = PasswordRevealMode.Visible;
+            }
+            else if (TBoxClaveEntrada.PasswordRevealMode == PasswordRevealMode.Hidden)
+            {
+                TBoxClaveEntrada.PasswordRevealMode = PasswordRevealMode.Visible;
+            }
+            else if (TBoxClaveEntrada.PasswordRevealMode == PasswordRevealMode.Visible)
+            {
+                TBoxClaveEntrada.PasswordRevealMode = PasswordRevealMode.Hidden;
+            }
+        }
+
+        // Encriptar Mensaje
+        private void ClickEncriptar(object sender, RoutedEventArgs e)
+        {
+            // Prepararar Black Box
+            OperacionCripto = new();
+            // Verificar Claves Existentes
+            if (TBoxClaveEntrada.Password.Trim().Length > 0)
+            {
+                TextoCodigo = TBoxClaveEntrada.Password.Trim();
+            }
+
+            // Verificar Texto existente
+            if (TBoxMensajeEntrada.Text.Trim().Length > 0)
+            {
+                TextoEntrada = TBoxMensajeEntrada.Text.Trim();
+            }
+
+            TextoSalida = OperacionCripto.Encriptar(TextoCodigo, TextoEntrada);
+            
+            TBoxMensajeSalida.Text = TextoSalida;
+        }
+
+        private void ClickDesEncriptar(object sender, RoutedEventArgs e)
+        {
+            // Prepararar Black Box
+            OperacionCripto = new();
+
+            // Verificar Claves Existentes
+            if (TBoxClaveEntrada.Password.Trim().Length > 0)
+            {
+                TextoCodigo = TBoxClaveEntrada.Password.Trim();
+            }
+
+            // Verificar Texto existente
+            if (TBoxMensajeSalida.Text.Trim().Length > 0)
+            {
+                TextoSalida = TBoxMensajeSalida.Text.Trim();
+            }
+
+            TextoEntrada = OperacionCripto.DesEncriptar(TextoCodigo, TextoSalida);
+
+            TBoxMensajeEntrada.Text = TextoEntrada;
+        }
+
+        // Boton de Salida
+        private void ClickSalir(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+
+    }
+}
